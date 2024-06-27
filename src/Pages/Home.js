91797e6db9652609bdcd03388f1c683eb0881dd6
RahmN <rahmanxrf@gmail.com>
2024-06-27 13:00:13 +0330

    debug

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -42,7 +42,7 @@ export const Home=()=>{
              {/* <!-- AboutUs -->  */}
             <div>
                 <Link  to='/AboutUs'>
-                <button className="ring-1 ring-blue-700 bg-blue-400 rounded-full p-1 m-2 focus:ring-blue-100 focus:ring-0">
+                <button className="ring-1 ring-blue-700 bg-blue-400 rounded-full p-1 m-1 focus:ring-blue-100 focus:ring-0">
                     <div className="ring-2 ring-stone-200 bg-gradient-to-b from-cyan-400 to-blue-500 p-4 rounded-full dark:ring-gray-800">
                         <span className="text-stone-200 ">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -59,7 +59,7 @@ export const Home=()=>{
             {/* <!-- Tutorial -->  */}
             <div>
                 <Link to='/Help'>
-                <button className="ring-1 ring-pink-700 bg-pink-400 rounded-full p-1 m-2 focus:ring-blue-100 focus:ring-0">
+                <button className="ring-1 ring-pink-700 bg-pink-400 rounded-full p-1 m-1 focus:ring-blue-100 focus:ring-0">
                     <div className="ring-2 ring-stone-200 bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500 p-4 rounded-full  dark:ring-gray-800">
                         <span className="text-stone-200 ">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -77,7 +77,7 @@ export const Home=()=>{
             <div>
                 <Link to='/Category'>
                 <button 
-                    className="ring-1 ring-green-400 bg-green-400 rounded-full p-1 m-2 focus:ring-blue-100 focus:ring-0">
+                    className="ring-1 ring-green-400 bg-green-400 rounded-full p-1 m-1 focus:ring-blue-100 focus:ring-0">
                     <div className="ring-2 ring-stone-200 bg-gradient-to-b from-indigo-500 from-5% via-sky-500 via-20% to-emerald-500 to-80% p-4 rounded-full  dark:ring-gray-800">
                         <span className="text-stone-200 ">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -94,7 +94,7 @@ export const Home=()=>{
              {/* <!-- Wallet -->  */}
             <div>
                 <Link to='/Wallet'>
-                <button className="ring-1 ring-blue-800 bg-blue-600 rounded-full p-1 m-2 focus:ring-blue-100 focus:ring-0">
+                <button className="ring-1 ring-blue-800 bg-blue-600 rounded-full p-1 m-1 focus:ring-blue-100 focus:ring-0">
                     <div className="ring-2 ring-stone-200 bg-gradient-to-b from-blue-300 to-blue-600 p-4 rounded-full  dark:ring-gray-800">
                         <span className="text-stone-200 ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8">
@@ -113,7 +113,7 @@ export const Home=()=>{
             <div>
                 <Link to='/FAQ'>
                 <button 
-                    className="ring-1 ring-purple-900 bg-purple-500 rounded-full p-1 m-2 focus:ring-blue-100 focus:ring-0">
+                    className="ring-1 ring-purple-900 bg-purple-500 rounded-full p-1 m-1 focus:ring-blue-100 focus:ring-0">
                     <div className="ring-2 ring-stone-200 bg-gradient-to-b from-purple-300 to-purple-700 p-4 rounded-full  dark:ring-gray-800">
                         <span className="text-stone-200 ">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -131,10 +131,14 @@ export const Home=()=>{
         </div>
          {/* <!--End circle menus -->  */}
         <div className=" justify-center mt-10">
-            <h1 className="text-lg m-5 font-bold"> فروشگاه شما</h1>
-            <img src={imgSrc} className="mx-auto" alt="AniDucks" />
+            <h1 className="text-lg text-center"> فروشگاه شما</h1>
+
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-20 path mx-auto">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+</svg>
+
             <h1 className="text-[30px] m-3 text-center ">خوش آمدید</h1>
-            <p className="text-sm me-7 opacity-50">در اینجا میتوانید فروشگاه خود را ببینید</p>
+            <p className="text-sm ms-7 opacity-50">در اینجا میتوانید فروشگاه خود را ببینید</p>
         </div>
         <Link to="/WelcomeMessage" className="p-5 bg-blue-800 rounded-[15px] m-3 w-[87%] flex justify-center mx-auto text-white">وارد شدن
             <span classNameName="ps-2">
