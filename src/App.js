5a835c687fad28ec08b5264955cc1590f8d617e2
RahmN <rahmanxrf@gmail.com>
2024-06-30 16:33:35 +0330

    back button add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import './App.css';
+// App.js
 
 import React, { useEffect, useState } from 'react';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import { AboutUs } from "./Pages/AboutUs";
-import { BotAdminvalidated } from "./Pages/BotAdminvalidated";
-import { BotInfo } from "./Pages/BotInfo";
-import { Category } from "./Pages/Category";
-import { Dashboard } from "./Pages/Dashboard";
-import { Error404 } from './Pages/404';
-import { FAQ } from "./Pages/FAQ";
-import { GettingStart } from "./Pages/GettingStart";
-import { Help } from "./Pages/Help";
-import { Home } from "./Pages/Home";
 import Preloader from './Pages/Preloader';
-import { Template } from "./Pages/Template";
-import ThemeSwitcher from "./Pages/ThemeSwitcher";
-import { Wallet } from "./Pages/Wallet";
-import { WelcomeMessage } from "./Pages/WelcomeMessage";
+import RoutesComponent from './Components/AppRoutes'; // Import the RoutesComponent
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -34,25 +20,7 @@ function App() {
       {isLoading ? (
         <Preloader />
       ) : (
-        <Router>
-          <ThemeSwitcher />
-        <Routes>
-          <Route path='/' element={ <Home />}/>
-          <Route path='/Home' element={ <Home />}/>
-          <Route path='/Home/WelcomeMessage' element={ <WelcomeMessage />}/>
-          <Route path='/Home/WelcomeMessage/GettingStart' element={ <GettingStart />}/>
-          <Route path='/Home/WelcomeMessage/GettingStart/BotInfo' element={ <BotInfo />}/>
-          <Route path='/Home/WelcomeMessage/GettingStart/BotInfo/Template' element={ <Template />}/>
-          <Route path='/Home/WelcomeMessage/GettingStart/BotInfo/Template/Bot&Adminvalidated' element={ <BotAdminvalidated />}/>
-          <Route path='/Home/WelcomeMessage/GettingStart/BotInfo/Template/Bot&Adminvalidated/Dashboard' element={ <Dashboard />}/>
-          <Route path='/Home/AboutUs' element={ <AboutUs />}/>
-          <Route path='/Home/Help' element={ <Help />}/>
-          <Route path='/Home/Category' element={ <Category />}/>
-          <Route path='/Home/Wallet' element={ <Wallet />}/>
-          <Route path='/Home/FAQ' element={ <FAQ />}/>
-          <Route path='*' element={ <Error404 />}/>
-        </Routes>
-        </Router>
+        <RoutesComponent />
       )}
     </section>
   );
---
+++ src/Components/AppRoutes.js
@@ -0,0 +1,45 @@
+// Routes.js
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { AboutUs } from '../Pages/AboutUs';
+import { BotAdminvalidated } from '../Pages/BotAdminvalidated';
+import { BotInfo } from '../Pages/BotInfo';
+import { Category } from '../Pages/Category';
+import { Dashboard } from '../Pages/Dashboard';
+import { Error404 } from '../Pages/404';
+import { FAQ } from '../Pages/FAQ';
+import { GettingStart } from '../Pages/GettingStart';
+import { Help } from '../Pages/Help';
+import { Home } from '../Pages/Home';
+import React from 'react';
+import { Template } from '../Pages/Template';
+import ThemeSwitcher from '../Pages/ThemeSwitcher';
+import { Wallet } from '../Pages/Wallet';
+import { WelcomeMessage } from '../Pages/WelcomeMessage';
+
+function RoutesComponent() {
+  return (
+    <Router>
+      <ThemeSwitcher />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/Home/WelcomeMessage' element={<WelcomeMessage />} />
+        <Route path='/Home/WelcomeMessage/GettingStart' element={<GettingStart />} />
+        <Route path='/Home/WelcomeMessage/GettingStart/BotInfo' element={<BotInfo />} />
+        <Route path='/Home/WelcomeMessage/GettingStart/BotInfo/Template' element={<Template />} />
+        <Route path='/Home/WelcomeMessage/GettingStart/BotInfo/Template/Bot&Adminvalidated' element={<BotAdminvalidated />} />
+        <Route path='/Home/WelcomeMessage/GettingStart/BotInfo/Template/Bot&Adminvalidated/Dashboard' element={<Dashboard />} />
+        <Route path='/Home/AboutUs' element={<AboutUs />} />
+        <Route path='/Home/Help' element={<Help />} />
+        <Route path='/Home/Category' element={<Category />} />
+        <Route path='/Home/Wallet' element={<Wallet />} />
+        <Route path='/Home/FAQ' element={<FAQ />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default RoutesComponent;
